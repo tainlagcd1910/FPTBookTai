f2ec27ba548be74bad66bcd62b138314600af77d
tainlagcd191069 <tainlagcd191069@fpt.edu.vn>
2022-09-03 01:33:05 +0700

    Add manager Account all User

--- FPTBookTai/Controllers/AdminController.cs
+++ FPTBookTai/Controllers/AdminController.cs
@@ -1,14 +1,39 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FPTBook.Areas.Identity.Data;
+using FPTBook.Data;
+using FPTBook.Models;
+
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace FPTBook.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly UserManager<FPTBookUser> _userManager;
+
+        private readonly FPTBookContext _context;
+
+        public AdminController(
+          UserManager<FPTBookUser> userManager, FPTBookContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Users()
+        {
+            var usersWithPermission = _userManager.Users;
+
+            return View(usersWithPermission);
         }
     }
 }
---
+++ FPTBookTai/ViewModel/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FPTBook.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}

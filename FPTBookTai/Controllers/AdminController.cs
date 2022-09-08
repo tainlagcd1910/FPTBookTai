97101428b463ab39de08ad5a74787b33614830ae
tainlagcd191069 <tainlagcd191069@fpt.edu.vn>
2022-09-08 15:32:17 +0700

    Change Password All User

--- FPTBookTai/Controllers/AdminController.cs
+++ FPTBookTai/Controllers/AdminController.cs
@@ -1,8 +1,8 @@
-﻿using FPTBook.Areas.Identity.Data;
+﻿
+using FPTBook.Areas.Identity.Data;
 using FPTBook.Data;
 using FPTBook.Models;
-
-
+using FPTBook.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -35,5 +35,43 @@ namespace FPTBook.Controllers
 
             return View(usersWithPermission);
         }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string id)
+        {
+            var user = _userManager.GetUserId(User);
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new ResetPasswordViewModel
+            {
+                Id = id,
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.NewPassword);
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return View();
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ FPTBookTai/ViewModel/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FPTBook.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
